52acf3e050ddac022c85a4d352b6e4a0e34da746
Ivan <login6453@ukr.net>
2019-05-10 21:36:56 +0300

    new test new commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
-//import Grid from 'react-bootstrap/Grid';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
-import styled from 'styled-components';
-import Header from "./components/header";
-import Main from "./components/main";
-import Menu from "./components/menu"
 
 class App extends Component {
   render(){
     return (
-  
+       <div className="App">
+           <h1>New test!</h1>
+       </div>
     );
   }
 }
