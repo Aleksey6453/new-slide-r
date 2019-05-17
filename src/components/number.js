a6f166afbeb11e85ea33c2ba7b109c4dba5da949
Ivan <login6453@ukr.net>
2019-05-17 16:42:24 +0300

    commit before color input

--- src/components/number.js
+++ src/components/number.js
@@ -1,11 +1,18 @@
 import React from "react";
+import "./number.css";
 
 const Number = (props) => {
+    const inputClasses = []
+
     return (
-       <div>
+       <div className="CarSt">
            <h2>Car name : {props.name}</h2>
            <p>Age car : {props.age}</p>
-           <input type="text" onChange={props.onChangeName} value={props.name} />
+           <input 
+                type="text" 
+                onChange={props.onChangeName}
+                 value={props.name}
+            />
            <button onClick={props.onDelete} className="btn margin-left">Del</button>
        </div>  
     )
